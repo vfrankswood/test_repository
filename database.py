95900b5415b2fe60d5337269e7b702de07ed4849
vfrankswood <vfrankswood@gmail.com>
2022-10-21 22:46:05 +0300

    VIDEOS fixed

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ articles = {
                           It seems that Russia will not survive after the defeat in Ukraine""",
     "WH": "POTUS congratulates Zelenskii",
     "SPORT": "Dynamo Kyiv lost 3 matches in UCL. Nothing new here",
-    "Films": 'The Last of us series is finally on air.',
+    "Videos": 'The Last of us series is finally on air.',
     "Economics": "China starves. They just bet on wrong part of the war. To be continued",
     "TRASH": '100000000000 Russians died in Ukraine'
             }
